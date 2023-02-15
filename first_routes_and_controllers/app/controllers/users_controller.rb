1aa2eae81dd1075d976e248fef81461e0304ba38
Charles Cruse <charle9@vt.edu>
2023-02-15 17:29:42 -0500

    doing post

--- first_routes_and_controllers/app/controllers/users_controller.rb
+++ first_routes_and_controllers/app/controllers/users_controller.rb
@@ -1,10 +1,14 @@
 class UsersController < ApplicationController
     def index
-        render plain: "I'm in the index action!"
+        @users = User.all
+        render json: @users
     end
 
     def create
-        render json: params
+        user = User.new(params.require(:user).permit(:name, :email))
+        # replace the `user_attributes_here` with the actual attribute keys
+        user.save!
+        render json: user
     end
 
     def show
