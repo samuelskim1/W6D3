8694210e28aa276ecb7c797b572b0d8ca3c6f16e
Charles Cruse <charle9@vt.edu>
2023-02-16 12:06:00 -0500

    finished artwork models and migration

--- first_routes_and_controllers/app/models/artwork.rb
+++ first_routes_and_controllers/app/models/artwork.rb
@@ -1,2 +1,10 @@
 class Artwork < ApplicationRecord
+    validates :title, :image_url, :artist_id, presence: true
+    validates :artist_id, uniqueness: {scope: :title, message: "An artist cannot have artworks with the same name"}
+    validates :image_url, uniqueness: true
+
+    belongs_to :artist,
+        primary_key: :id,
+        foreign_key: :artist_id,
+        class_name: :User
 end
--- first_routes_and_controllers/db/schema.rb
+++ first_routes_and_controllers/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_02_16_161258) do
+ActiveRecord::Schema[7.0].define(version: 2023_02_16_170012) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -22,6 +22,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_16_161258) do
     t.datetime "updated_at", null: false
     t.index ["artist_id", "title"], name: "index_artworks_on_artist_id_and_title", unique: true
     t.index ["artist_id"], name: "index_artworks_on_artist_id"
+    t.index ["image_url"], name: "index_artworks_on_image_url", unique: true
   end
 
   create_table "users", force: :cascade do |t|
