e44967e16e643541111d09fc143152bdfbe55efb
Charles Cruse <charle9@vt.edu>
2023-02-16 10:53:50 -0500

    finished first routes

--- first_routes_and_controllers/app/controllers/users_controller.rb
+++ first_routes_and_controllers/app/controllers/users_controller.rb
@@ -5,13 +5,43 @@ class UsersController < ApplicationController
     end
 
     def create
-        user = User.new(params.require(:user).permit(:name, :email))
+        user = User.new(user_params)
         # replace the `user_attributes_here` with the actual attribute keys
-        user.save!
-        render json: user
+        if user.save!
+            render json: user
+        else
+            render json: user.errors.full_messages, status: 422
+        end
     end
 
     def show
-        render json: params
+        @user = User.find(params[:id])
+        render json: @user
     end
+
+    def update
+        @user = User.find(params[:id])
+
+        if @user.update(user_params)
+            redirect_to user_url(@user.id)
+        else
+            render json: @user.errors.full_messages, status: 422
+        end
+    end
+
+    def destroy
+        @user = User.find(params[:id])
+
+        if @user.destroy
+            render json: User.all
+        else
+            render json: @user.errors.full_messages, status: 422
+        end
+    end
+
+
+    def user_params
+        params.require(:user).permit(:name, :email)
+    end
+
 end
